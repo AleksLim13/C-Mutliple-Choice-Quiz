70f29f416a526c205c27a10d1c597a9f7d796b30
Ally Cat <alekslim13@gmail.com>
2021-07-31 17:24:12 -0400

    Update _2018Movies.cs

--- _2018Movies.cs
+++ _2018Movies.cs
@@ -1,1 +1,50 @@
 
+ class _2018Movies
+    {
+        private string[] names = {
+            "The Mule",
+            "I Am Not a Witch",
+            "The Favourite",
+            "Mission: Impossible – Fallout",
+            "Bisbee ’17",
+            "Burning",
+            "Leave No Trace",
+            "A Prayer Before Dawn",
+            "The Endless",
+            "Paddington 2",
+            "Filmworker"
+       };
+
+        private string[] questions = {
+          "This is a 2018 movie that stars Clint Eastwood and Bradley Cooper and is a drama/crime movie",
+          "This is a 2018 movie that stars Maggie Mulubwa and Henry B.J. Phiri and is a drama film",
+          "This is a 2018 movie that stars Emma Stone and Henry Hoult and is a drama / comedy film",
+          "This is a 2018 movie that stars Rebecca Ferguson and Henry Cavill and is a action / thriller film",
+          "This is a 2018 movie that stars Mike Anderson and Laurie McKenna and is a documentary / western film",
+          "This is a 2018 movie that stars Steven Yeun and Jeon Jong Seo and is a mystery / drama film",
+          "This is a 2018 movie that stars Thomasin Mckenzie and Ben Foster and is a drama film",
+          "This is a 2018 movie that stars Joe Cole and Pornchanok Mabklang and is a action / drama film",
+          "This is a 2018 movie that stars Justin Benson and Callie Hernandez and is a horro / sci-fi film",
+          "This is a 2018 movie that stars Hugh Grant and Sally Hawkins and is a family / adventure film",
+          "This is a 2018 movie that stars Brian Jamieson and Marie Richardson and is a documentary film"
+        };
+
+
+        public _2018Movies()
+        {
+
+        }//End C:*
+
+        public string[] GetMovieNames()
+        {
+
+            return this.names;
+        }//End M:*
+
+        public string[] GetMovieQuestions()
+        {
+
+            return this.questions;
+        }//End M:*
+
+    }//End CL:*
